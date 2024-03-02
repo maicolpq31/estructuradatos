6d4b164d8f7575e4ecf639a31a7f048c5b58538c
michaelpq31 <pardo.maicol.mp@gmail.com>
2024-03-02 18:54:18 -0500

    actual commit

--- Estructura_Datos/app.py
+++ Estructura_Datos/app.py
@@ -1,25 +1,47 @@
-"""Crear una lista de diccionarios donde cada diccionario contiene la información de un estudiante.
-
-Usar un list comprehension para crear una lista de objetos de la clase Student a partir de la lista de diccionarios.
-
-Usar otro list comprehension para filtrar los estudiantes, excluyendo los que tienen una nota promedio menor que un umbral dado.
-Usar un dictionary comprehension para crear un diccionario de estudiantes que tienen una nota promeidio por encima de un umbral dado,
-con los nombre de los estudiantes como claves y el objeto Student como valor."""
+"""
+"""
 from Estructura_Datos.ejercicioclase import Student
 
-#asi se crea un diccionario
+#Crear una lista de diccionarios donde cada diccionario contiene la información de un estudiante.
+
+#asi se crea un diccionario,  una lista adentro las claves: y valores
 data = [
-    {"name": "Pedro", "age": 18, "grades": [2.6, 1.5, 4.2]},
-    {"name": "Maria", "age": 23, "grades": [1.5, 3.8, 4.5]},
+    {"name": "Pedro", "age": 18, "grades": [3.3, 4.1, 5]},
+    {"name": "Maria", "age": 23, "grades": [3.4, 3.8, 4.5]},
     {"name": "Sandro", "age": 19, "grades": [3.2, 3.8, 4.1]}
 ]
 
-students: list[Student] = []
+#Usar un list comprehension para crear una lista de objetos de la clase Student a partir de la lista de diccionarios.
+
+students: list[Student] = []      #Hacerlo en listcomprehention
 for register in data:
     student = Student(register["name"], register["age"]) #objeto estudiante
     for grade in register["grades"]:
         student.add_grade(grade)
     students.append(student)
 
+threshold = 3.5#limite del promedio mostrado en pantalla
+
 for st in students:
+    print(st)
+
+#Usar otro list comprehension para filtrar los estudiantes, excluyendo los que tienen una nota promedio menor que un umbral dado.
+filtered_students: list[Student] = []
+for st in students:
+    if st.average_grade() >= threshold:
+        filtered_students.append(st)
+
+
+#Usar un dictionary comprehension para crear un diccionario de estudiantes que tienen una nota promedio por encima de un umbral dado, con los nombre de los estudiantes como claves y el objeto Student como valor.
+dict_students: dict[str, Student] = {}
+for st in students:
+    if st.average_grade() >= threshold:
+        dict_students[st.name] = st
+
+print("\ndiccionario de estudiantes")
+print(dict_students)
+
+
+print(f"Students with average grade over {threshold}")
+for st in filtered_students:
     print(st)
--- Estructura_Datos/ejercicioclase.py
+++ Estructura_Datos/ejercicioclase.py
@@ -1,25 +1,25 @@
+
+
+
+
+
 """Crear una clase Student que tiene los siguientes atributos: name, age y grades (una lista de números)
-Crear un constructor que inicialice los atributos
-
-Crear un método llamado add_grade que recibe un número como parámetro y lo agrega a la lista grades.
-
-Crear otro método llamado average_grade que calcule y retorne la nota promedio de la lista de notas grades
-"""
+Crear un constructor que inicialice los atributos"""
 class Student:
     def __init__(self, name: str, age: int):
         self.name: str = name
         self.age: int = age
         self.grades: list[float] = []
 
-
-    def add_grade(self, grade: float):
+    """Crear un método llamado add_grade que recibe un número como parámetro y lo agrega a la lista grades."""
+    def add_grade(self, grade: float):   #grade = calificacion
         self.grades.append(grade)
 
-
+    """Crear otro método llamado average_grade que calcule y retorne la nota promedio de la lista de notas grades"""
     def average_grade(self) -> float:
         return sum(self.grades) / len(self.grades)
 
-
+    #metodo especial para printear esos resultados
     def __str__(self):
         return f"{self.name}, edad {self.age}, -promedio {self.average_grade()}"
 

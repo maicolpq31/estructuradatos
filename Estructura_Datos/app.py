50e6d9163b57fc56bad4ef95588ddc4717a7423e
michaelpq31 <pardo.maicol.mp@gmail.com>
2024-03-02 16:56:06 -0500

    tercer comit

---
+++ Estructura_Datos/app.py
@@ -0,0 +1,23 @@
+"""Crear una lista de diccionarios donde cada diccionario contiene la información de un estudiante.
+
+Usar un list comprehension para crear una lista de objetos de la clase Student a partir de la lista de diccionarios.
+
+Usar otro list comprehension para filtrar los estudiantes, excluyendo los que tienen una nota promedio menor que un umbral dado.
+Usar un dictionary comprehension para crear un diccionario de estudiantes que tienen una nota promeidio por encima de un umbral dado,
+con los nombre de los estudiantes como claves y el objeto Student como valor."""
+from Estructura_Datos.ejercicioclase import Student
+
+#asi se crea un diccionario
+data = [
+    {"name": "Pedro", "age": 18, "grades": [2.6, 1.5, 4.2]},
+    {"name": "Maria", "age": 23, "grades": [1.5, 3.8, 4.5]},
+    {"name": "Sandro", "age": 16, "grades": [3.2, 3.8, 4.1]}
+]
+
+students: list[Student] = []
+for register in data:
+    student = Student(register["name"], register["age"], register["grades"]) #objeto estudiante
+    students.append(student)
+
+for st in students:
+    print(st)
--- Estructura_Datos/ejercicioclase.py
+++ Estructura_Datos/ejercicioclase.py
@@ -4,23 +4,27 @@ Crear un constructor que inicialice los atributos
 Crear un método llamado add_grade que recibe un número como parámetro y lo agrega a la lista grades.
 
 Crear otro método llamado average_grade que calcule y retorne la nota promedio de la lista de notas grades
-Crear una lista de diccionarios donde cada diccionario contiene la información de un estudiante.
-Usar un list comprehension para crear una lista de objetos de la clase Student a partir de la lista de diccionarios.
-Usar otro list comprehension para filtrar los estudiantes, excluyendo los que tienen una nota promedio menor que un umbral dado.
-Usar un dictionary comprehension para crear un diccionario de estudiantes que tienen una nota promeidio por encima de un umbral dado,
-con los nombre de los estudiantes como claves y el objeto Student como valor."""
+"""
+
+
 
 class Student:
-    def __init__(self, name: str, age: int):
+    def __init__(self, name: str, age: int, grades: list[float]):
         self.name: str = name
         self.age: int = age
-        self.grades: list[float] = []
-
+        self.grades: list[float] = grades
 
     def add_grade(self, grade: float):
         self.grades.append(grade)
 
-
     def average_grade(self) -> float:
         return sum(self.grades) / len(self.grades)
+
+
+    def __str__(self):
+        return f"{self.name}, edad {self.age}, notas {self.grades}"
+
+
+    def promedio(self):
+        return
 
